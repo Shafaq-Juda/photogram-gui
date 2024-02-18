930a2077650dadf8a41109b9fcbb3a0e96efb987
Your Name <you@example.com>
2024-02-18 07:34:35 +0000

    Gave show details a dynamic route, then defined that route in routes and set a controller and action method for that route and in that method, rendered a .html.erb template for that path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,7 @@ class UsersController < ApplicationController
     render ({ :template => "user_templates/index" })
   end
 
+  def show
+    render ({ :template => "user_templates/show" })
+  end
 end
