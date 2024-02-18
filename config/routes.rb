ce5a214e168309f057a290358d8d4b849378f261
Your Name <you@example.com>
2024-02-18 07:48:38 +0000

    populated show.html.erb for the user/:path_username, with h1, dl, form, h2 and table

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  get("/", { :controller => "homepage", :action => "index" })
+
   get("/users", { :controller => "users", :action => "index" })
 
   get("/users/:path_username", { :controller => "users", :action => "show" })
